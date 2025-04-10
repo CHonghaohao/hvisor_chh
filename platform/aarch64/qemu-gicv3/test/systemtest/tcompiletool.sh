f7204ad75c111f53f96f895c09f9dbe3796bef77
CHonghao <chonghao1108@163.com>
2025-04-10 10:37:56 +0800

    test28

--- platform/aarch64/qemu-gicv3/test/systemtest/tcompiletool.sh
+++ platform/aarch64/qemu-gicv3/test/systemtest/tcompiletool.sh
@@ -7,15 +7,15 @@ set -e  # Exit immediately if any command fails
     cd ./platform/aarch64/qemu-gicv3/image/virtdisk
     mkdir rootfs/
     sudo mount rootfs1.ext4 rootfs
-    sudo ls -ld rootfs/home/arm64
+    sudo ls -l rootfs/home/arm64
     pwd
     git clone https://github.com/CHonghaohao/linux_5.4.git
     git clone https://github.com/syswonder/hvisor-tool.git
     cd hvisor-tool
     make all ARCH=arm64 LOG=LOG_WARN KDIR=../../linux_5.4
-    # sudo cp ./tools/hvisor ../rootfs/home/arm64/
-    # sudo cp ./driver/hvisor.ko ../rootfs/home/arm64/
     cd ..
+    sudo cp ./hvisor-tool/tools/hvisor ./rootfs/home/arm64/
+    sudo cp ./hvisor-tool/driver/hvisor.ko ./rootfs/home/arm64/
     sudo umount rootfs
     
 )
